06975863a67d3b9116504af85fb3a9c731e76dfb
Đàm Quốc Dân <124369155+damquocdan@users.noreply.github.com>
2023-07-28 16:21:15 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._04Cshap
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a =20, b =30;
+            double area;
+            Utility u= new Utility();
+            Console.WriteLine("Trước khi hoán vị a={0},b={1}",a,b);
+            u.SwapFake(a, b);
+            Console.WriteLine("Sau khi hoán vị đang Fage a{0},b={1}", a, b);
+            u.SwapReally(ref a,ref b);
+            Console.WriteLine("Sau khi hoán vị dang really a={0},b={1}", a, b);
+            u.AreaCircle(100,out area);
+            Console.WriteLine("Chu vi hình tròn bán kính 100 là :{0}",area);
+            Console.ReadKey();
+        }
+        
+    }
+}
---
+++ Utility.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3._04Cshap
+{
+    internal class Utility
+    { 
+        // Không sử dụng ref
+        public void SwapFake(int a, int b)
+        {
+            int tg = a;
+            a=b;
+            b=tg;
+        }
+        //Sử dụng ref cho hàm số
+        public void SwapReally(ref int a, ref int b)
+        {
+            int tg = a;
+            a=b;
+            b=tg;
+        }
+        // sử dụng out cho tham số
+        public void AreaCircle(double radius, out double area)
+        {
+            area = 2*Math.PI*radius;
+        }
+    }
+}
